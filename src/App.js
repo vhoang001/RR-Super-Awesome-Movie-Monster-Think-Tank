bd8109fe909415a2bf18b8dee37d5e51c974b28c
Van Hoang <hoangduykhanhvan@gmail.com>
2024-06-02 11:29:09 -0700

    Activity: Combine Music Library Features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import Loader from 'react-loader-spinner';
 
 function App() {
+  const AboutUs = React.lazy(() => import('./AboutUs'))
+  const Packages = React.lazy(() => import('./Packages'))
+
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
-          <AboutUs />
-          <Packages />
+        <Suspense fallback={<Loader 
+            type="Bars" 
+            color="#FF00FF" 
+            height={200} 
+            width={200} 
+            style={{margin: "0 auto"}}/>}>
+          <h1>Hello World</h1>
+        </Suspense>        
       </div>
     </div>
   );
